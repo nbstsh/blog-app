b1edf7052609c17d3bad40a4d5da5bfe404f9e89
Satoshi Inaba <satoshi@SatoshinoMacBook-puro.local>
2018-12-11 16:30:12 +0900

    create blog model

--- routes/blog.js
+++ routes/blog.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const router = express.Router();
+const Blog = require('../models/blog');
 
 //INDEX
 router.get('/', function (req, res) {
